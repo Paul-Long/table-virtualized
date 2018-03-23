1616217b5e8d0f568a5e204ae6acf15bfa763c23
baolong.hou <baolong.hou@sumscope.com>
2018-03-23 18:15:49 +0800

    update demo data

--- client/containers/app/index.js
+++ client/containers/app/index.js
@@ -17,7 +17,21 @@ const columns = [
     dataIndex: 'a',
     key: 'a',
     width: '20%',
-    bodyStyle: {background: '#1E1F17', color: '#FF9200'}
+    bodyStyle: {background: '#1E1F17', color: '#FF9200'},
+    children: [
+      {
+        title: 'title11', align: 'left', dataIndex: 'a1', width: '10%', children: [
+          {title: 'title111', align: 'left', dataIndex: 'a11', width: '5%'},
+          {title: 'title112', align: 'left', dataIndex: 'a12', width: '5%'}
+        ]
+      },
+      {
+        title: 'title12', align: 'left', dataIndex: 'a2', width: '10%', children: [
+          {title: 'title121', align: 'left', dataIndex: 'a21', width: '5%'},
+          {title: 'title122', align: 'left', dataIndex: 'a22', width: '5%'}
+        ]
+      }
+    ]
   },
   {
     title: 'title2',
@@ -25,7 +39,15 @@ const columns = [
     dataIndex: 'b',
     key: 'b',
     width: '30%',
-    bodyStyle: {background: '#122024', color: '#11A1FF'}
+    bodyStyle: {background: '#122024', color: '#11A1FF'},
+    // children: [
+    //   {
+    //     title: 'title11', align: 'left', dataIndex: 'b1', width: '15%'
+    //   },
+    //   {
+    //     title: 'title12', align: 'left', dataIndex: 'b2', width: '15%'
+    //   }
+    // ]
   },
   {
     title: 'title5',
@@ -41,7 +63,19 @@ const columns = [
 const data = [];
 
 for (let i = 0; i < 2000; i++) {
-  data.push({a: '4.88', b: '1000', c: '200', d: '17:20:03', key: i + ''})
+  data.push({
+    a: '4.88',
+    b: '1000',
+    b1: '1000',
+    b2: '1500',
+    c: '200',
+    d: '17:20:03',
+    key: i + '',
+    a11: '200',
+    a12: '3.25',
+    a21: '200',
+    a22: '3.25'
+  })
 }
 
 class App extends React.Component {
@@ -66,9 +100,10 @@ class App extends React.Component {
           columns={columns}
           dataSource={data}
           fixedHeader
+          showHeader
           bordered
-          width='90%'
-          height='90%'
+          width={'calc(100% - 10px)'}
+          height={'calc(100% - 10px)'}
         />
       </div>
     )
