6a0ee4ee39e04efc1c2079466dd3c7f70883a88d
baolong.hou <baolong.hou@sumscope.com>
2018-04-10 18:08:16 +0800

    update demo data

--- client/containers/app/index.js
+++ client/containers/app/index.js
@@ -4,78 +4,71 @@ import './style.css';
 
 const columns = [
   {
-    title: 'title4',
+    title: '第一列',
     align: 'left',
     dataIndex: 'key',
-    key: 'd',
-    width: '20%',
+    order: true,
+    width: 400,
     bodyStyle: {background: '#121A18', color: '#F9C152'}
   },
   {
-    title: 'title1',
-    align: 'left',
-    dataIndex: 'a',
-    key: 'a',
-    width: '20%',
+    title: '第二列',
+    dataIndex: 'key0',
+    width: 400,
     bodyStyle: {background: '#1E1F17', color: '#FF9200'},
-    children: [
-      {
-        title: 'title11', align: 'left', dataIndex: 'a1', width: '100', children: [
-          {title: 'title111', align: 'left', dataIndex: 'a11', width: '5%'},
-          {title: 'title112', align: 'left', dataIndex: 'a12', width: '5%'}
-        ]
-      },
-      {
-        title: 'title12', align: 'left', dataIndex: 'a2', width: '10%', children: [
-          {title: 'title121', align: 'left', dataIndex: 'a21', width: '5%'},
-          {title: 'title122', align: 'left', dataIndex: 'a22', width: '5%'}
-        ]
-      }
-    ]
-  },
-  {
-    title: 'title2',
-    align: 'left',
-    dataIndex: 'b',
-    key: 'b',
-    width: '30%',
-    bodyStyle: {background: '#122024', color: '#11A1FF'},
     // children: [
     //   {
-    //     title: 'title11', align: 'left', dataIndex: 'b1', width: '15%'
+    //     title: 'title11', dataIndex: 'a1', width: '100', children: [
+    //       {title: 'title111', align: 'left', dataIndex: 'a11', width: '5%'},
+    //       {title: 'title112', align: 'right', dataIndex: 'a12', width: '5%'}
+    //     ]
     //   },
     //   {
-    //     title: 'title12', align: 'left', dataIndex: 'b2', width: '15%'
+    //     title: 'title12', dataIndex: 'a2', width: '10%', children: [
+    //       {title: 'title121', align: 'left', dataIndex: 'a21', width: '5%'},
+    //       {title: 'title122', align: 'right', dataIndex: 'a22', width: '5%'}
+    //     ]
     //   }
     // ]
   },
   {
-    title: 'title5',
+    title: '第三列',
+    dataIndex: 'key1',
+    width: 400,
+    bodyStyle: {background: '#122024', color: '#11A1FF'},
+    // children: [
+    //   {
+    //     title: 'title21', align: 'left', dataIndex: 'b1', width: '15%'
+    //   },
+    //   {
+    //     title: 'title22', align: 'right', dataIndex: 'b2', width: '15%'
+    //   }
+    // ]
+  },
+  {
+    title: '第四列',
     align: 'left',
-    dataIndex: 'c',
-    key: 'e',
-    width: '10%',
+    dataIndex: 'key2',
+    width: 400,
     bodyStyle: {background: '#121A18', color: '#F9C152'}
   },
-  {title: 'title6', align: 'left', dataIndex: 'd', key: 'f', bodyStyle: {background: '#121A18', color: '#7B8280'}},
+  {
+    title: '第五列',
+    align: 'left',
+    dataIndex: 'key3',
+    width: 400,
+    bodyStyle: {background: '#121A18', color: '#7B8280'}
+  },
 ];
 
 const data = [];
 
-for (let i = 0; i < 5000; i++) {
-  data.push({
-    a: '4.88',
-    b: '1000',
-    b1: '1000',
-    b2: '1500',
-    c: '200',
-    d: '17:20:03',
-    key: i + '',
-    a11: '200',
-    a12: '3.25',
-    a21: '200',
-    a22: '3.25'
-  })
+for (let i = 0; i < 10000; i++) {
+  const row = {key: `${i}`};
+  for (let j = 0; j < 10; j++) {
+    row[`key${j}`] = Math.random().toString(36).substr(2);
+  }
+  data.push(row);
 }
 
 class App extends React.Component {
@@ -83,6 +76,10 @@ class App extends React.Component {
     super(props);
     this.timer = 0;
   }
+
+  state = {
+    data: data
+  };
 
   componentWillMount() {
     this.timer = (new Date()).getTime();
@@ -98,13 +95,18 @@ class App extends React.Component {
       <div>
         <Table
           columns={columns}
-          dataSource={data}
+          dataSource={this.state.data}
           fixedHeader
           showHeader
           bordered
           width={'calc(100% - 10px)'}
           height={'calc(100% - 10px)'}
           footer={() => '加载更多'}
+          onSort={(column, order) => {
+            // console.log(order);
+            const data = this.state.data;
+            this.setState({data: [...data].reverse()});
+          }}
         />
       </div>
     )
